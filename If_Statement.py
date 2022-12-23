43d3f754094fe456f5c72760356a4fbb71fbbddb
Anietie Lawrence Hanson Essien <arrhnnie@gmail.com>
2022-12-23 02:14:06 +0100

    Add files via upload

---
+++ If_Statement.py
@@ -0,0 +1,9 @@
+#Look what I did guys! I'm so proud of myself! :)
+x=int(input("Your first number is: "))
+print (x)
+y=int(input("Your second number is: "))
+print (y)
+if x>y:
+    print("Your first number is greater than your second number.")
+else:
+    print("Your second number is greater than your first number.")
